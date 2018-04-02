9d03d000b9a4b9d66661fc3672826f34ca2c6f24
Travis CI User <travis@example.org>
2018-04-02 16:40:34 +0000

    Documentation for rust-lang-nursery/rand@903d94d

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rand"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"rand/jitter/enum.TimerError.html\" title=\"enum rand::jitter::TimerError\">TimerError</a>&gt; for <a class=\"struct\" href=\"rand/struct.Error.html\" title=\"struct rand::Error\">Error</a>",synthetic:false,types:["rand_core::error::Error"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/rand_core/trait.BlockRngCore.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rand"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/rand-core/0.1/rand_core/trait.BlockRngCore.html\" title=\"trait rand_core::BlockRngCore\">BlockRngCore</a> for <a class=\"struct\" href=\"rand/prng/chacha/struct.ChaChaCore.html\" title=\"struct rand::prng::chacha::ChaChaCore\">ChaChaCore</a>",synthetic:false,types:["rand::prng::chacha::ChaChaCore"]},{text:"impl <a class=\"trait\" href=\"https://docs.rs/rand-core/0.1/rand_core/trait.BlockRngCore.html\" title=\"trait rand_core::BlockRngCore\">BlockRngCore</a> for <a class=\"struct\" href=\"rand/prng/hc128/struct.Hc128Core.html\" title=\"struct rand::prng::hc128::Hc128Core\">Hc128Core</a>",synthetic:false,types:["rand::prng::hc128::Hc128Core"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
